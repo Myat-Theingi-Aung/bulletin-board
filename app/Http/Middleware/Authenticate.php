2c4957ac788f6768ae00737eabe5492c053ce0f0
Myat-Theingi-Aung <myattheingiaung1720@gmail.com>
2023-09-12 09:12:45 +0630

    add authorization header in every request

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     */
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
+    }
+
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if ($token = $request->cookie('token')) {
+            $request->headers->set('Authorization', 'Bearer ' . $token);
+        }
+
+        $this->authenticate($request, $guards);
+
+        return $next($request);
     }
 }
