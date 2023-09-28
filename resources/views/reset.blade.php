371d149c9d425ed6a05f7bb9f83da86689870e25
Myat-Theingi-Aung <myattheingiaung1720@gmail.com>
2023-09-28 09:21:13 +0630

    change port number

--- resources/views/reset.blade.php
+++ resources/views/reset.blade.php
@@ -6,7 +6,7 @@
 <body>
   <h1 class="mb-4">Welcome to Bulletin_Board, {{ $user->name }}</h1>
   <p>Your name is: {{ $user->name }}</p>
-  <a href="http://127.0.0.1:5173/reset/{{ $token }}">http://127.0.0.1:5173/reset/{{ $token }}</a>
+  <a href="http://127.0.0.1:3000/reset/{{ $token }}">http://127.0.0.1:3000/reset/{{ $token }}</a>
   <p>Click the link and reset password</p>
   <p>Thanks for joining and have a great day!</p>
 </body>
